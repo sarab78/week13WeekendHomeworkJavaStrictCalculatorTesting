d011d87da4c592ac1777effb51bc84c0998b8e09
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-08-03 15:55:05 +0100

    Homework Completed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,89 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  //Do the number buttons update the display of the running total?
+  it('it should update the display of the running total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number5')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('15')
+  })
+
+  //Do the arithmetical operations update the display with the result of the operation?
+ it('it should update the display with the result of the operation', function(){
+   running_total = element(by.css('#running_total'));
+   element(by.css('#number2')).click();
+   element(by.css('#operator_add')).click();
+   element(by.css('#number4')).click();
+   element(by.css('#operator_equals')).click();
+   expect(running_total.getAttribute('value')).to.eventually.equal('6');
+
+ })
+ //Can multiple operations be chained together?
+  it('it should chain multiple operations together', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click()
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  //Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+    it('it should provide output for negative numbers', function(){
+      element(by.css('#number4')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number8')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('-4')
+    })
+
+    it('it should provide output for decimal numbers', function(){
+      element(by.css('#number1')).click();
+      element(by.css('#number5')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number4')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('3.75')
+    })
+
+    it('it should provide output for very large numbers', function(){
+      element(by.css('#number5')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_add')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#number5')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('7500000')
+  })
+
+// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass.
+
+it('it should return infinity in exceptional circumstances', function(){
+  element(by.css('#number5')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('Infinity')
+})
+
+
+
+
 });
